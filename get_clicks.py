4fc2f31269feb2f754ec42312d9ed343aef3a4b8
Gloridust <Gloridust@gmail.com>
2024-04-16 19:03:23 +0800

    Update get_clicks.py

--- get_clicks.py
+++ get_clicks.py
@@ -1,6 +1,9 @@
 import requests
 
-url = "https://www.yibinu.edu.cn/system/resource/code/news/click/dynclicks.jsp?clickid=62031&owner=1397404744&clicktype=wbnews"
-header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0"}
-res = requests.get(url=url, headers=header)
-print(res.text)
+def get_clicks(clickid):
+    url = f"https://www.yibinu.edu.cn/system/resource/code/news/click/dynclicks.jsp?clickid={clickid}&owner=1397404744&clicktype=wbnews"
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0"}
+    res = requests.get(url=url, headers=header)
+    print(res.text)
+
+get_clicks("62031")
