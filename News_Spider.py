89f1107ee21cfa8974d96465cf8e6162176eaaee
Gloridust <Gloridust@gmail.com>
2024-04-16 19:49:22 +0800

    Update News_Spider.py

--- News_Spider.py
+++ News_Spider.py
@@ -1,8 +1,8 @@
 import requests
 from lxml import etree
-import json
 import csv
 import os
+import get_article 
 
 def get_page_links():
     page_links=[]
@@ -48,8 +48,8 @@ def get_news_lists(page_link):
         # print(article_link)
     return(article_list)
 
-def get_article(news_link):
-    pass
+# def get_article(news_link):
+#     pass
 
 def write_to_csv(article_title, article_date, article_clicks, article_text):
     # 检查文件是否存在，如果不存在则创建文件并写入表头
@@ -72,5 +72,5 @@ if __name__ == "__main__":
         #传入页面链接，返回一个含有各新闻链接的列表
         for news_link in news_list:
             #获取一篇新闻的 题目 日期 点击量 正文 
-            article_title,article_date,article_clicks,article_text = get_article(news_link)
+            article_title,article_date,article_clicks,article_text = get_article.get_article_main(news_link)
             write_to_csv(article_title,article_date,article_clicks,article_text)
