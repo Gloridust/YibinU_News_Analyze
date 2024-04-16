3724330cce2902af6554f4a692e439a99086b6e5
Gloridust <Gloridust@gmail.com>
2024-04-16 19:01:18 +0800

    Update get_clicks.py

--- get_clicks.py
+++ get_clicks.py
@@ -1,6 +1,4 @@
 import requests
-from lxml import etree
-import json
 
 url = "https://www.yibinu.edu.cn/system/resource/code/news/click/dynclicks.jsp?clickid=62031&owner=1397404744&clicktype=wbnews"
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0"}
