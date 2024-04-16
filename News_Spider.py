6a407b699a3719b256a45bdb4e2d8a2488a278ba
Gloridust <Gloridust@gmail.com>
2024-04-16 17:10:05 +0800

    Update News_Spider.py

--- News_Spider.py
+++ News_Spider.py
@@ -8,7 +8,21 @@ def get_page_links():
     pass
 
 def get_news_lists(page_link):
-    pass
+    article_list=[]
+    url = page_link
+    header = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0"}
+    res = requests.get(url=url, headers=header)
+    print(res.status_code)
+    res.encoding = "utf-8"
+    html = res.text
+    html = etree.HTML(html)
+    srcs = html.xpath("//div[@class='title']/a/@href")
+    for i in srcs:
+        article_link = "https://www.yibinu.edu.cn" + i[2::]
+        article_list.append(article_link)
+        # print(article_link)
+    print(article_list)
 
 def get_article(news_link):
     pass
@@ -20,18 +34,19 @@ def write_to_csv(article_title, article_date, article_clicks, article_text):
         writer = csv.writer(file)
         if not file_exists:
             writer.writerow(['article_title', 'article_date', 'article_clicks', 'article_text'])
-        
         # 将文章信息写入CSV文件
         writer.writerow([article_title, article_date, article_clicks, article_text])
 
 
 if __name__ == "__main__":
-
-    page_links = get_page_links() #获取页面列表，返回一个含有各页面链接的列表
-
-    for page_link in page_links: #从列表中遍历出每一个链接
-        news_list = get_news_lists(page_link) #传入页面链接，返回一个含有各新闻链接的列表
-
-        for news_link in news_list: #从列表便利出每一篇新闻的链接
-            article_title,article_date,article_clicks,article_text = get_article(news_link) #获取一篇新闻的 题目 日期 点击量 正文 
+    #获取页面列表，返回一个含有各页面链接的列表
+    page_links = get_page_links()
+    #从列表中遍历出每一个链接
+    for page_link in page_links:
+        #传入页面链接，返回一个含有各新闻链接的列表
+        news_list = get_news_lists(page_link)
+        #传入页面链接，返回一个含有各新闻链接的列表
+        for news_link in news_list:
+            #获取一篇新闻的 题目 日期 点击量 正文 
+            article_title,article_date,article_clicks,article_text = get_article(news_link)
             write_to_csv(article_title,article_date,article_clicks,article_text)
